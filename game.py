f7826a5e39a08fae62361b0abeb0334bf43c1f3a
youszef <zouhari_y@hotmail.com>
2020-12-28 14:42:19 +0100

    add missing method to instantiate game from dict

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
-from typing import List, Any
-
-import player
+import card
+import player as player_module
 import deck
 import commands.pile
 
@@ -15,13 +14,41 @@ class Game(object):
         self._game_over = False
         self._current_player = None
 
+    @property
+    def active_players(self):
+        return self._active_players
+
+    @property
+    def pile(self):
+        return self._pile
+
+    @property
+    def removed_cards(self):
+        return self._removed_cards
+
+    @property
+    def winners(self):
+        return self._winners
+
+    @property
+    def cards(self):
+        return self._cards
+
+    @property
+    def current_player(self):
+        return self._current_player
+
+    @property
+    def game_over(self):
+        return self._game_over
+
     def start_new_game(self, players, player_name_key=None, player_id_key=None):
         if player_name_key and player_id_key:
             for player_dict in players:
-                self._active_players.append(player.Player(player_dict[player_name_key], player_dict[player_id_key]))
+                self._active_players.append(player_module.Player(player_dict[player_name_key], player_dict[player_id_key]))
         else:
             for player_name in players:
-                self._active_players.append(player_name)
+                self._active_players.append(player_module.Player(player_name))
 
         self.__deal_cards()
 
@@ -31,7 +58,7 @@ class Game(object):
         if not all(player.is_ready for player in self._active_players): return False
         if self._current_player: return self._current_player
 
-        self._current_player = min(self._active_players, key=lambda p: min(p.hand, key=lambda c: c.order_value))
+        self._current_player = min(self._active_players, key=lambda player: min(player.hand, key=lambda c: c.order_value))
         return self._current_player
 
     def throw_cards(self, player, thrown_cards):
@@ -72,6 +99,18 @@ class Game(object):
             'winners': list(map(lambda player: player.to_dict(), self._winners)),
             'game_over': self._game_over
         }
+
+    @classmethod
+    def from_dict(cls, game_dict):
+        game = cls()
+        game._active_players = list(
+            map(lambda player_dict: player_module.Player.from_dict(player_dict), game_dict['active_players']))
+        game._cards = list(map(lambda card_dict: card.Card.from_dict(card_dict), game_dict['cards']))
+        game._pile = list(map(lambda card_dict: card.Card.from_dict(card_dict), game_dict['pile']))
+        game._removed_cards = list(map(lambda card_dict: card.Card.from_dict(card_dict), game_dict['removed_cards']))
+        game._current_player = player_module.Player.from_dict(game_dict['current_player'])
+        game._winners = list(map(lambda player_dict: player_module.Player.from_dict(player_dict), game_dict['winners']))
+        game._game_over = game_dict['game_over']
 
     def __deal_cards(self):
         for player in self._active_players:
@@ -124,9 +163,8 @@ class Game(object):
         self.__verify_and_set_game_over()
 
     def __verify_and_set_game_over(self):
-        remaining_players = filter(lambda p: p not in self._winners, self._active_players)
+        remaining_players = list(filter(lambda p: p not in self._winners, self._active_players))
         if len(remaining_players) > 1: return None
 
-        if remaining_players[0]:self._winners.append(remaining_players[0])
+        if remaining_players[0]: self._winners.append(remaining_players[0])
         self._game_over = True
-
